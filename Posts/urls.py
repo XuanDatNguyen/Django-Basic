d8139a7c0b569af6b0ec5e669146c5acb73b10cf
XuanDatNguyen <nguyenxuandat.bqp@gmail.com>
2022-12-23 13:26:34 +0700

    update post rest api

--- Posts/urls.py
+++ Posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.posts, name='posts')
+    path('posts/', views.PostAPIView.as_view(), name='posts'),
+    path('posts/<str:pk>', views.PostAPIView.as_view(), name='posts')
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, authentication
+from rest_framework.decorators import permission_classes, authentication_classes
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
-def posts(request):
-    return HttpResponse("Posts runing...")
+
+
+@permission_classes([permissions.IsAuthenticated])
+@authentication_classes([authentication.BasicAuthentication])
+class PostAPIView(APIView):
+    def get(self, request):
+        data = Post.objects.all()
+        serializer = PostSerializer(data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({"message": "Created successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+
+    def put(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(
+                instance=post, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "Updated successfully", "data": serializer.data}, status=status.HTTP_200_OK)
+        except:
+            return Response({"message": "Post not found!!!"}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            post.delete()
+            return Response({"message": "Deleted"}, status=status.HTTP_200_OK)
+        except:
+            return Response({"message": "Post not found!!!"}, status=status.HTTP_404_NOT_FOUND)
